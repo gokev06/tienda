a96b317bed1d8cf4f67edaa1fd88113cdf5a82c5
gokev06 <kevin2006gonzalez@gmail.com>
2023-07-13 09:12:10 -0500

    carrito

---
+++ js/index.js
@@ -0,0 +1,94 @@
+let  boton1 = document.getElementById("carrito1");
+let pElement= document.getElementById("contador")
+let  contador=0;
+
+boton1.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton2 = document.getElementById("carrito2");
+
+
+boton2.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton3 = document.getElementById("carrito3");
+
+boton3.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton4 = document.getElementById("carrito4");
+
+
+boton4.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton5 = document.getElementById("carrito5");
+
+
+boton5.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton6 = document.getElementById("carrito6");
+
+boton6.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+
+let  boton6a = document.getElementById("carrito6a");
+
+boton6a.onclick= function(){
+    contador--;
+    pElement.textContent=contador;
+}
+
+
+let  boton5a = document.getElementById("carrito5a");
+
+boton5a.onclick= function(){
+    contador--;
+    pElement.textContent=contador;
+}
+
+
+let  boton4a = document.getElementById("carrito4a");
+
+boton4a.onclick= function(){
+    contador--;
+    pElement.textContent=contador;
+}
+
+
+let  boton3a = document.getElementById("carrito3a");
+
+boton3a.onclick= function(){
+    contador--;
+    pElement.textContent=contador;
+}
+
+
+let  boton2a = document.getElementById("carrito2a");
+
+boton2a.onclick= function(){
+    contador--;
+    pElement.textContent=contador;
+}
+
+
+let  boton1a = document.getElementById("carrito1a");
+
+boton1a.onclick= function(){
+    contador--;
+    pElement.textContent=contador;
+}
